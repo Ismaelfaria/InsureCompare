ee4682ce9a418586019998bf54b0730d2b605949
limaismael8901 <limaismael8901@gmail.com>
2024-12-03 18:54:01 -0300

    🚀💻Initial commit: project structure and dependencies

---
+++ src/test/java/com/project/InsureCompare/InsureCompareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.InsureCompare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsureCompareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
