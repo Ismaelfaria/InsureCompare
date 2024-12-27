1adc9fa55cb4228bf50e2da66fb6eb44b90c95d8
limaismael8901 <limaismael8901@gmail.com>
2024-12-27 05:58:47 -0300

    🎉✅ Correção Realizada: Comunicação com o banco de dados MySQL estabelecida com sucesso
    Problema resolvido! A conexão com o MySQL foi restabelecida após análise
    e correções no ambiente Docker. 🚀
    
    🛠️ Ações realizadas:
    - Reinstalação do container MySQL para garantir uma configuração limpa.
    - Isolamento do erro e identificação de que o Docker estava mapeando o
    container para a porta 52814 em vez da porta 3306 esperada.
    - Ajuste no mapeamento de portas para corrigir a discrepância.
    
    💡 Resultado: A aplicação Spring Boot agora conecta-se ao banco de dados
    MySQL sem erros de comunicação. O ambiente está funcional e pronto para
    desenvolvimento e testes! 🌟

--- src/main/java/com/project/InsureCompare/InsureCompareApplication.java
+++ src/main/java/com/project/InsureCompare/InsureCompareApplication.java
@@ -2,12 +2,14 @@ package com.project.InsureCompare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 public class InsureCompareApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InsureCompareApplication.class, args);
+			SpringApplication.run(InsureCompareApplication.class, args);
 	}
 
 }
